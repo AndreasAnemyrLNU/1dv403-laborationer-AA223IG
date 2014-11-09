a1154c8f44839a8465d95cac20c6d0daa0fe38fc
Andréas Anemyr <aa223ig@student.lnu.se>
2014-11-09 12:37:38 +0000

    changed to getRandomInte()

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,12 +2,13 @@
 
 window.onload = function(){
 	
-	var secret = getRandomArbitrary(0,51); // Detta tal behöver bytas ut mot ett slumpat tal.
+	var secret = getRandomInt(0,51); // Detta tal behöver bytas ut mot ett slumpat tal.
 	
 	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-	// Returns a random number between min (inclusive) and max (exclusive)
-	function getRandomArbitrary(min, max) {
-	return Math.random() * (max - min) + min;
+	// Returns a random integer between min (included) and max (excluded)
+	// Using Math.round() will give you a non-uniform distribution!
+	function getRandomInt(min, max) {
+  	return Math.floor(Math.random() * (max - min)) + min;
 	}
 	
 	

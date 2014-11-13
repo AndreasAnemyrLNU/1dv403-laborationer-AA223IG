d5253011fd25e1abf3b2e858ff497364bebbe4e9
Andréas Anemyr <aa223ig@student.lnu.se>
2014-11-13 08:06:24 +0000

    throw errors....

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -8,7 +8,20 @@ var makePerson = function(persArr){
         var maxAge;  
         
             persArr.forEach(function(item){
-                arrayOfAges.push(item.age);
+                try
+                {
+                    if(typeof item.age === 'number' && item.age % 1 === 0)
+                    {
+                        arrayOfAges.push(item.age);
+                    }
+                    else{
+                        throw "Fel! obj.age av annan typ än int";
+                    }
+                }
+                catch(err)
+                {
+                    console.log(err);
+                }
             });
             
         arrayOfAges.sort();
@@ -33,11 +46,19 @@ var makePerson = function(persArr){
         };
 //*************************************************
         var calcAverageAge = function(persArr){
-         
-        var arrayOfAges = [];
+        
+        var count = 0; 
+        var sum = 0;
         var averageAge;
             
-            //persArr.forEach(){};
+            persArr.forEach(function(item){
+                sum += item.age;
+                count++;
+            });
+        
+        averageAge = sum / count;
+
+        return Math.round(averageAge);        
         };
 //*************************************************        
         var names = function(persArr){
@@ -60,7 +81,7 @@ var makePerson = function(persArr){
 };
 //*************************************************
 //*************************************************
-var data = [{name: "John Häggerud", age: 37}, 
+var data = [{name: "John Häggerud", age: 37.6}, 
             {name: "Johan Leitet", age: 36}, 
             {name: "Mats Loock", age: 46}];
 

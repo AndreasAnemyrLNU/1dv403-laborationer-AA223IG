04179c5bfc3b55a3cdd44aa675b63847df9fedcf
Andréas Anemyr <aa223ig@student.lnu.se>
2014-11-13 07:13:48 +0000

    start with calcAverageAge()

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,50 +1,65 @@
 "use strict";
 
 var makePerson = function(persArr){
-
-    var calcMaxAge = function(persArr){
+//*************************************************
+        var calcMaxAge = function(persArr){
         
-    var arrayOfAges = [];    
-    var MaxAge;  
+        var arrayOfAges = [];    
+        var maxAge;  
         
-        persArr.forEach(function(item){
-            arrayOfAges.push(item.age);
-        });
+            persArr.forEach(function(item){
+                arrayOfAges.push(item.age);
+            });
+            
+        arrayOfAges.sort();
+        maxAge = arrayOfAges[arrayOfAges.length -1];
         
-    arrayOfAges.sort();
-    MaxAge = arrayOfAges[arrayOfAges.length -1];
-    return MaxAge;   
+        return maxAge;   
+        };
+//*************************************************    
+        var calcMinAge = function(persArr){
         
-    };
-    var calcMinAge = function(persArr){
-        return 360;
-    };
-
-    var calcAverageAge = function(persArr){
+        var arrayOfAges = [];
+        var minAge;
         
-    };
-    
-    var names = function(persArr){
-        result = [];
-        persArr.forEach(function(item){
-            result.push(item.name);
-        });
-    //stackoverflow.com/questions/14528998/is-there-any-javascript-library-with-implementations-of-sort-methods-for-alphabe
-    result = result.sort(function(a,b){return a.localeCompare(b)});
-    result = result.join(", ");
-    return result;
-    };
-
-    return  {
+            persArr.forEach(function(item){
+                arrayOfAges.push(item.age);
+            });
+        
+        arrayOfAges.sort();
+        minAge = arrayOfAges[0];
+        
+        return minAge;
+        };
+//*************************************************
+        var calcAverageAge = function(persArr){
+         
+        var arrayOfAges = [];
+        var averageAge;
+            
+            //persArr.forEach(){};
+        };
+//*************************************************        
+        var names = function(persArr){
+            result = [];
+            persArr.forEach(function(item){
+                result.push(item.name);
+            });
+        //stackoverflow.com/questions/14528998/is-there-any-javascript-library-with-implementations-of-sort-methods-for-alphabe
+        result = result.sort(function(a,b){return a.localeCompare(b)});
+        result = result.join(", ");
+        return result;
+        };
+//*************************************************
+    return{
                 averageAge: calcAverageAge(persArr),
                 maxAge:     calcMaxAge(persArr),
                 minAge:     calcMinAge(persArr),
                 names:      names(persArr)
             };
-
-
 };
-
+//*************************************************
+//*************************************************
 var data = [{name: "John Häggerud", age: 37}, 
             {name: "Johan Leitet", age: 36}, 
             {name: "Mats Loock", age: 46}];

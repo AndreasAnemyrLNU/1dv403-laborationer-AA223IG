0e3670765a11601259061dfa1ebb008a39c90e61
Andréas Anemyr <aa223ig@student.lnu.se>
2014-11-15 00:31:57 +0000

    addEvent and testing....

--- 2-labbymezzage/js/run.js
+++ 2-labbymezzage/js/run.js
@@ -7,11 +7,16 @@ var run = {
     
     init:       function(){
         
-        var mess = new Message("Testmeddelande", new Date());
-        alert(mess);
-        alert(mess.getText());
-        mess.setText("En annan text");
-        alert(mess);
+        
+        
+        
+        
+        
+        //var mess = new Message("Testmeddelande", new Date());
+        //alert(mess);
+        //alert(mess.getText());
+        //mess.setText("En annan text");
+        //alert(mess);
         
     }
     

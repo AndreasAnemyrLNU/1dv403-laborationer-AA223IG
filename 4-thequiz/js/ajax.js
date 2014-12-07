e82c3ce188979d5599ff0459068e1aa3d8a94ca7
Andréas Anemyr <aa223ig@student.lnu.se>
2014-12-07 23:24:49 +0000

    some progress

--- 4-thequiz/js/ajax.js
+++ 4-thequiz/js/ajax.js
@@ -3,39 +3,40 @@
 var xhr = new XMLHttpRequest();
 
 xhr.onreadystatechange = function(){
+	
+	if(xhr.readyState === 4)
+	{
+
+
 	var ret = JSON.parse(xhr.responseText);
 
 	console.log("id: "+ret.id);
 	console.log("question: "+ret.question);
 	console.log("nextURL: "+ret.nextURL);
 	console.log("message: "+ret.message);
+	
+	}
 
 };
 
 
-
-xhr.open("GET", "http://vhost3.lnu.se:20080/question/1", false);
+xhr.open("GET", "http://vhost3.lnu.se:20080/question/1", true);
 
 
 xhr.send(null);
 
 
-xhr.abort();
-
-
-
 
 var xhr2 = new XMLHttpRequest();
 
-xhr2.onreadystatechange = function(){
+	if(xhr2.readyState === 4)
+	{
+		xhr2.onreadystatechange = function(){
 
+			console.log(xhr2.responseText);
 
-	console.log(xhr2.responseText);
-
-
-};
-
-
+		};
+	}
 
 
 xhr2.open("POST", "http://vhost3.lnu.se:20080/answer/1", true);
@@ -44,10 +45,10 @@ xhr2.setRequestHeader('Content-type','application/json');
 
 
 
-xhr2.send("{answer : This URL only accept POST}");
+xhr2.send("{\"answer\": \"2\"}");
 
 
-xhr.abort();
+
 
 
 
